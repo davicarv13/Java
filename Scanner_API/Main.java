599a805bc4b08f8eac7d6027460ed97bee0d703f
davicarv13 <davicarv13@gmail.com>
2019-08-17 21:08:26 -0300

    Update Square.java, Main.java

--- Scanner_API/Main.java
+++ Scanner_API/Main.java
@@ -4,10 +4,13 @@ public class Main{
 
 	public static void main( String[] args ){
 
+		//Creates a scanner to obtain data from terminal prompt
 		Scanner sc = new Scanner( System.in );
 
 		Square square1 = new Square( sc.nextFloat());
 
 		System.out.println( "Square 1 area: " +square1.calcArea());
+
+
 	}
 }
