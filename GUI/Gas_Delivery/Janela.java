883067ee30fda856eae7a466c8a403dcab9cef01
Davi Carvalho <davicarv13@gmail.com>
2019-10-27 12:12:11 -0300

    Update Gas_Delivery subdirectory

---
+++ GUI/Gas_Delivery/Janela.java
@@ -0,0 +1,16 @@
+package Gas_Delivery;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Janela extends JFrame{
+	public Janela(){
+		this.setTitle("Delivery de Gás");
+		this.setSize(500, 300);
+		this.setVisible(true);
+		this.setResizable(true);	
+	}
+	
+}
---
+++ GUI/Gas_Delivery/Main.java
@@ -0,0 +1,7 @@
+package Gas_Delivery;
+
+public class Main{
+	public static void main(String[] args){
+		Janela objJanela = new Janela();
+	}
+}
